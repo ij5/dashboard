04bb236bd1ffa3237c9b5edd14ec7e2e335034bf
Jaehee Lee <55607202+ij5@users.noreply.github.com>
2024-06-14 20:32:31 +0900

    remove print panic

--- src/errors.rs
+++ src/errors.rs
@@ -7,13 +7,13 @@ use crate::{log, tui};
 /// This replaces the standard color_eyre panic and error hooks with hooks that
 /// restore the terminal before printing the panic or error.
 pub fn install_hooks() -> color_eyre::Result<()> {
-    let (panic_hook, eyre_hook) = HookBuilder::default().into_hooks();
+    let (_panic_hook, eyre_hook) = HookBuilder::default().into_hooks();
 
     // convert from a color_eyre PanicHook to a standard panic hook
-    let panic_hook = panic_hook.into_panic_hook();
+    // let panic_hook = panic_hook.into_panic_hook();
     panic::set_hook(Box::new(move |panic_info| {
         tui::restore().unwrap();
-        panic_hook(panic_info);
+        // panic_hook(panic_info);
         let time = SystemTime::now();
         let _ = log::println(&format!("Panic: {:?}: {:?}", time, panic_info));
     }));
